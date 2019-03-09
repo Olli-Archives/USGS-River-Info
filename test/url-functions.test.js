c3b8d221b31cdec6d4f16870d46f39c401a055fe
ollinieminen <otn987@gmail.com>
2019-03-08 16:57:03 -0800

    create APU URL passes

--- test/url-functions.test.js
+++ test/url-functions.test.js
@@ -23,14 +23,33 @@ test('create query from params', assert => {
     const actual = createURL(params);
     //assert
 
-    assert.equal(expected, actual);
+    assert.equal(actual, expected);
 
 });
 
 test('create URL for fetch', assert =>{
+
+    function createURL(query, params){
+        const URL_BASE = 'https://waterservices.usgs.gov/nwis/iv/?';
+        const ApiURL = new URL(URL_BASE);
+        ApiURL.searchParams.set('format', 'json');
+        ApiURL.searchParams.set('sites', params.siteID);
+        ApiURL.searchParams.set('parameterCd', params.parameterCd);
+        ApiURL.searchParams.set('siteType', 'ST');
+        ApiURL.searchParams.set('siteStatus', 'all');
+        return ApiURL.toString();
+    }
+
     //arrange
-    const expected = 
+    const expected = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=14301000&parameterCd=00060%2C00065&siteType=ST&siteStatus=all';
+    const query = 'format=json&sites=14301000&parameterCd=00060%2C00065&siteType=ST&siteStatus=all';
+    const params =
+    {   
+        parameterCd: '00060,00065',
+        siteID: 14301000
+    };
     //act
+    const actual = createURL(query, params);
     //assert
-    assert.equal(expected, actual);
+    assert.equal(actual, expected);
 });
