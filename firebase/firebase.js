36c19446f6dcbd23237001df9e524193c5ad3547
ollinieminen <otn987@gmail.com>
2019-03-12 12:10:36 -0700

    add header container for html docs

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,8 @@
 const config = {
     apiKey: 'AIzaSyCa8hLfYIVwRjhFt4dfOKrGSPhkDKexRiw',
-    authDomain: 'usgs-282a6.firebaseapp.com'
+    authDomain: 'usgs-282a6.firebaseapp.com',
+    databaseURL: 'https://usgs-282a6.firebaseio.com',
+    projectId: 'usgs-282a6',
 };
 
 firebase.initializeApp(config);
